99c26621b0c5dc604e779398a250cc921ceb6dee
Andrew King <kinga0879@gmail.com>
2025-11-08 20:01:50 -0500

    Update boot script handling and configs for i.MX8MP
    Changed default device tree in imx8mp-var-dart.conf to use the Symphony board. Refined kernel config to only disable the problematic HDMI RX CEC component and keep VIDEO_IMX_CAPTURE enabled. Fixed reconfig.sh to use the correct path for sysvinit-inittab. Enhanced create_bootpartition to improve boot script generation, add fallback logic, and ensure board-specific boot scripts are renamed to boot.scr if present.

--- configs/poky/reconfig.sh
+++ configs/poky/reconfig.sh
@@ -30,7 +30,7 @@ if [ $1 = fixsys ]; then
 	echo export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/lib | sudo tee -a $RFSDIR/etc/profile 1>/dev/null
     fi
     sudo ln -sf /sbin/init $RFSDIR/init
-    sudo mkdir -p $RFSDIR/etc/rpm-postinsts && sudo cp -f $FBDIR/src/system/poky/100-sysvinit-inittab $RFSDIR/etc/rpm-postinsts
+    sudo mkdir -p $RFSDIR/etc/rpm-postinsts && sudo cp -f $FBDIR/src/system/100-sysvinit-inittab $RFSDIR/etc/rpm-postinsts
     sudo sed -i '/start_getty 115200  vt102/d' $RFSDIR/etc/inittab && sudo sed -i '/38400 tty1/d' $RFSDIR/etc/inittab
     cd $RFSDIR/etc/rcS.d && sudo ln -sf ../init.d/run-postinsts S99run-postinsts && cd - && \
     if ! grep -q ^S1 $RFSDIR/etc/inittab; then \
